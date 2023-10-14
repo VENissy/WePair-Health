219e037a21a7d428b22783d149f70cec61e6c861
V Noyes <131526323+VENissy@users.noreply.github.com>
2023-10-14 12:52:11 -0400

    Create Blog.cs

---
+++ dotnet/Blog.cs
@@ -0,0 +1,33 @@
+using WePair.Models.Domain.Users;
+using System;
+
+namespace WePair.Models.Domain.Blogs
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+
+        public LookUp Category { get; set; }
+
+        public BaseUser Author { get; set; }
+
+        public string Title { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Content { get; set; }
+
+        public bool IsPublished { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime DateModified { get; set; }
+
+        public DateTime DatePublish { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+    }
+}
